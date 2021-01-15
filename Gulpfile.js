7dd8a6fbb25caadfe9f2744d7135c65935ff09f1
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-15 17:46:38 +0100

    Updated build to include base64 conversion

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,38 +1,45 @@
-const { src, dest, watch, series, parallel } = require('gulp')
+const { src, dest, watch, series } = require('gulp')
 const pug = require('gulp-pug')
 const less = require('gulp-less')
 const cleanCSS = require('gulp-clean-css')
+const base64 = require('gulp-base64-inline')
 const clean = require('gulp-clean')
-
-function renderTemplates () {
-  return src('src/views/*.pug')
-    .pipe(pug({}))
-    .pipe(dest('build'))
-}
 
 function compileLess () {
   return src('src/views/styles/main.less')
     .pipe(less({}))
     .pipe(cleanCSS({}))
-    .pipe(dest('build/styles'))
+    // convert all font references to inline base64
+    .pipe(base64('../static/fonts'), {
+      includeMime: false,
+      prefix: 'url(data:font/ttf;base64,',
+      suffix: ')'
+    })
+    .pipe(dest('src'))
 }
 
-function copyFonts () {
-  return src('src/views/static/fonts/*')
-    .pipe(dest('build/fonts/'))
+function renderTemplates () {
+  return src('src/views/*.pug')
+    .pipe(pug({}))
+    // convert all image references to inline base64
+    .pipe(base64('./static/imgs', {
+      prefix: '',
+      suffix: ''
+    }))
+    .pipe(dest('build'))
 }
 
-function copyImgs () {
-  return src('src/views/static/imgs/*')
-    .pipe(dest('build/imgs/'))
-}
-
-function cleanUp () {
+function preCleanUp () {
   return src('build', { read: false, allowEmpty: true })
     .pipe(clean())
 }
 
-const runner = series(cleanUp, parallel(copyFonts, copyImgs), compileLess, renderTemplates)
+function postCleanUp () {
+  return src('src/main.css', { read: false, allowEmpty: true })
+    .pipe(clean())
+}
+
+const runner = series(preCleanUp, compileLess, renderTemplates, postCleanUp)
 
 exports.default = process.env.GULP_BUILD
   ? runner // prod
