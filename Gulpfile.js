0f5d7a7e1dcf1497405eda19898129be118c66ea
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-15 11:42:27 +0100

    Added task for copying font when building

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,7 @@
-const { src, dest, parallel, watch, series } = require('gulp')
+const { src, dest, watch, series } = require('gulp')
 const pug = require('gulp-pug')
 const less = require('gulp-less')
-const cleanCSS = require('gulp-clean-css');
+const cleanCSS = require('gulp-clean-css')
 
 function renderTemplates () {
   return src('src/views/*.pug')
@@ -16,7 +16,12 @@ function compileLess () {
     .pipe(dest('build/styles'))
 }
 
-const runner = series(compileLess, renderTemplates)
+function copyFonts () {
+  return src('src/views/fonts/*')
+    .pipe(dest('build/fonts/'))
+}
+
+const runner = series(copyFonts, compileLess, renderTemplates)
 
 exports.default = process.env.GULP_BUILD
   ? runner // prod
