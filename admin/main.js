1eb64dedd528504a3d73deaa4e65f37d43b13311
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-18 15:54:03 +0100

    Testing upload

--- admin/main.js
+++ admin/main.js
@@ -1,14 +1,40 @@
-const imgPicker = document.querySelector('#img')
+const imgPicker = document.querySelector('#img-picker')
 const thumbnail = document.querySelector('#thumbnail')
+const uploadButton = document.querySelector('#upload-button')
 
 imgPicker.addEventListener('change', e => {
   const file = e.target.files[0]
-  const reader = new window.FileReader()
-  reader.addEventListener('load', function () {
-    // convert image file to base64 string
-    thumbnail.src = reader.result
-  }, false)
-  if (file) {
+  const key = file.name
+  if (!file) {
+    thumbnail.src = ''
+  } else {
+    const reader = new window.FileReader()
+    reader.addEventListener('load', function () {
+      // convert image file to base64 string
+      const content = reader.result
+      storeData({ key, content })
+    }, false)
     reader.readAsDataURL(file)
   }
 })
+
+uploadButton.addEventListener('click', e => {
+  const { key, content } = getData()
+  const init = {
+    method: 'PUT',
+    body: JSON.stringify({ key, content })
+  }
+  window.fetch('http://localhost:3101', init)
+})
+
+function storeData ({ key, content }) {
+  thumbnail.src = content
+  thumbnail['data-key'] = key
+}
+
+function getData () {
+  return {
+    key: thumbnail['data-key'],
+    content: thumbnail.src
+  }
+}
