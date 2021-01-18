07d386bf02398c608156e3982cf6029551bd6128
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-18 15:53:44 +0100

    Added CORS

--- admin/services/images/index.js
+++ admin/services/images/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3101
 
 require('dotenv').config()
 
 app.use(express.json({ limit: '25mb', extended: false }))
+app.use(cors())
 
 app.use((req, res, next) => {
   process.chdir(process.env.ROOT)
