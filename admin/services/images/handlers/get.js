097c894dc68c929d19fb7582dd50c08b8e87131b
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-18 14:37:40 +0100

    Moved utility into handler

--- admin/services/images/handlers/get.js
+++ admin/services/images/handlers/get.js
@@ -1,5 +1,30 @@
-const getFiles = require('../utils/get-files.js')
+const { readdirSync, readFileSync, statSync } = require('fs')
+const { join, basename } = require('path')
+const { lookup } = require('mime-types')
 
 module.exports = (req, res) => {
   res.json(getFiles())
+}
+
+function getFiles (dir = process.cwd()) {
+  return readdirSync(dir)
+    .map(file => {
+      const filePath = join(dir, file)
+      const stat = statSync(filePath)
+      return stat.isDirectory()
+        ? {
+            key: file,
+            content: getFiles(filePath)
+          }
+        : {
+            key: basename(filePath),
+            content: getContent(filePath)
+          }
+    })
+}
+
+function getContent (file) {
+  const mimeType = lookup(file)
+  const content = readFileSync(file, 'base64')
+  return `data:${mimeType};base64,${content}`
 }
