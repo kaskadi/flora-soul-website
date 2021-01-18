2e7757acbac9d34b4f2d7188697e3f5c9d0c661b
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-18 14:12:57 +0100

    Added error handling

--- admin/services/images/handlers/delete.js
+++ admin/services/images/handlers/delete.js
@@ -1,9 +1,13 @@
-const { unlinkSync } = require('fs')
+const { unlinkSync, existsSync } = require('fs')
 const getPath = require('../utils/get-path.js')
 
 module.exports = (req, res) => {
   const { key } = req.body
   const filePath = getPath(key)
-  unlinkSync(filePath)
-  res.status(200).send(`File ${key} successfully deleted!`)
+  if (!existsSync(filePath)) {
+    res.status(404).send(`No file named ${key} found...`)
+  } else {
+    unlinkSync(filePath)
+    res.status(200).send(`File ${key} successfully deleted!`)
+  }
 }
--- admin/services/images/handlers/patch.js
+++ admin/services/images/handlers/patch.js
@@ -1,12 +1,16 @@
-const { readFileSync, writeFileSync, unlinkSync } = require('fs')
+const { readFileSync, writeFileSync, unlinkSync, existsSync } = require('fs')
 const getPath = require('../utils/get-path.js')
 
 module.exports = (req, res) => {
   const { key, oldKey } = req.body
   const filePath = getPath(key)
   const oldFilePath = getPath(oldKey)
-  const oldFile = readFileSync(oldFilePath, 'base64')
-  unlinkSync(oldFilePath)
-  writeFileSync(filePath, oldFile, 'base64')
-  res.status(201).send(`File ${key} successfully updated!`)
+  if (!existsSync(oldFilePath)) {
+    res.status(404).send(`No file named ${oldKey} found...`)
+  } else {
+    const oldFile = readFileSync(oldFilePath, 'base64')
+    unlinkSync(oldFilePath)
+    writeFileSync(filePath, oldFile, 'base64')
+    res.status(201).send(`File ${key} successfully updated!`)
+  }
 }
