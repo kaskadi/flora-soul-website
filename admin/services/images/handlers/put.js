07cbd11b58b891248b00c1efe7a49fbb2378e2a1
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-18 14:27:39 +0100

    Implemented missing sub folders creation

--- admin/services/images/handlers/put.js
+++ admin/services/images/handlers/put.js
@@ -1,10 +1,22 @@
-const { writeFileSync } = require('fs')
+const { writeFileSync, existsSync, mkdirSync } = require('fs')
 const getPath = require('../utils/get-path.js')
 
 module.exports = (req, res) => {
   const { key, content } = req.body
+  createStructure(key)
   const filePath = getPath(key)
   const fileContent = content.replace(/^data:[a-z]+\/[a-z]+;base64,/, '') // strip off base64 URL header from string to retrieve actual encoded data
   writeFileSync(filePath, fileContent, 'base64')
   res.status(201).send(`File ${key} successfully saved!`)
+}
+
+function createStructure (key) {
+  const dirs = key.split('/').slice(0, -1)
+  for (let i = 0; i < dirs.length; i++) {
+    let dirPath = dirs.slice(0, i + 1).join('/')
+    dirPath = getPath(dirPath)
+    if (!existsSync(dirPath)) {
+      mkdirSync(dirPath)
+    }
+  }
 }
