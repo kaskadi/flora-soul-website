6971bf9ae052f92609c72c6575f13c8fd0b4addc
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-18 15:15:59 +0100

    Added directory removal support

--- admin/services/images/handlers/delete.js
+++ admin/services/images/handlers/delete.js
@@ -1,11 +1,11 @@
-const { unlinkSync, existsSync } = require('fs')
+const { rmSync, existsSync } = require('fs')
 
 module.exports = (req, res) => {
   const { key } = req.body
   if (!existsSync(key)) {
     res.status(404).send(`No file named ${key} found...`)
   } else {
-    unlinkSync(key)
+    rmSync(key, { force: true, recursive: true })
     res.status(200).send(`File ${key} successfully deleted!`)
   }
 }
