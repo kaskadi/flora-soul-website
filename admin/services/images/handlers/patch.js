7b585f81f3b39928117a1e219583c3c0367d6f76
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-19 18:32:34 +0100

    Fixed file rename logic

--- admin/services/images/handlers/patch.js
+++ admin/services/images/handlers/patch.js
@@ -1,13 +1,11 @@
-const { readFileSync, writeFileSync, unlinkSync, existsSync } = require('fs')
+const { renameSync, existsSync } = require('fs')
 
 module.exports = (req, res) => {
   const { key, oldKey } = req.body
   if (!existsSync(oldKey)) {
     res.status(404).send(`No file named ${oldKey} found...`)
   } else {
-    const oldFile = readFileSync(oldKey, 'base64')
-    unlinkSync(oldKey)
-    writeFileSync(key, oldFile, 'base64')
+    renameSync(oldKey, key)
     res.status(201).send(`File ${key} successfully updated!`)
   }
 }
