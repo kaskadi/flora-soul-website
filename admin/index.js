1fa36df11967ee33fa1d56a62357eb710bfe541d
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-19 15:35:26 +0100

    Added path parameter to control view display

--- admin/index.js
+++ admin/index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3100
 
+const { existsSync } = require('fs')
+
 app.set('view engine', 'pug')
 app.use(express.static('views'))
 
 app.get('/', (req, res) => {
   res.render('index', {})
+})
+
+app.get('/:view', (req, res) => {
+  const { view } = req.params
+  if (!existsSync(`views/${view}.pug`)) {
+    res.status(404).send('This page does not exist')
+  } else {
+    res.render(view, {})
+  }
 })
 
 app.listen(port, () => {
