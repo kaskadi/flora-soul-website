55ef35436786c98ed94fb096aa520da58f58f0aa
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-25 12:00:19 +0100

    Fixed race condition

--- admin/services/images/index.js
+++ admin/services/images/index.js
@@ -12,21 +12,13 @@ require('dotenv').config()
 app.use(express.json({ limit: '25mb', extended: false }))
 app.use(cors())
 
-app.use((req, res, next) => {
-  process.chdir(process.env.ROOT)
-  next()
-})
+process.chdir(process.env.ROOT)
 
 app.get('/', require('./handlers/get.js'))
 app.get('/download', require('./handlers/download.js'))
 app.post('/create', require('./handlers/create.js'), broadcast)
 app.post('/rename', require('./handlers/rename.js'), broadcast)
 app.post('/delete', require('./handlers/delete.js'), broadcast)
-
-app.use((req, res, next) => {
-  process.chdir(__dirname)
-  next()
-})
 
 const server = app.listen(port, () => {
   console.log(`Images API running http://localhost:${port}`)
