d42681324e2ded4d85006422da5f582b4b566952
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-26 10:52:34 +0100

    Filtering out hidden files

--- admin/services/images/handlers/utils/get-files.js
+++ admin/services/images/handlers/utils/get-files.js
@@ -6,27 +6,39 @@ module.exports = (dir = '') => {
   if (dir.length === 0) {
     dir = process.cwd()
   }
-  const files = readdirSync(dir)
-    .map(file => {
-      const filePath = join(dir, file)
-      return {
-        key: file,
-        content: statSync(filePath).isDirectory() ? null : getContent(filePath)
-      }
-    })
-    .sort((fileA, fileB) => {
-      const isContentString = obj => typeof obj.content === 'string'
-      if (!isContentString(fileA) && isContentString(fileB)) {
-        return -1
-      } else if (isContentString(fileA) && !isContentString(fileB)) {
-        return 1
-      } else {
-        return 0
-      }
-    })
   return {
     dir: dir.replace(process.cwd(), ''),
-    files
+    files: readdirSync(dir)
+      .filter(filterFiles)
+      .map(getData(dir))
+      .sort(sortFiles)
+  }
+}
+
+function filterFiles (file) {
+  // filter out hidden file
+  return !file.startsWith('.')
+}
+
+function getData (dir) {
+  return file => {
+    const filePath = join(dir, file)
+    return {
+      key: file,
+      content: statSync(filePath).isDirectory() ? null : getContent(filePath)
+    }
+  }
+}
+
+function sortFiles (fileA, fileB) {
+  // sort so that folders come before files
+  const isContentString = obj => typeof obj.content === 'string'
+  if (!isContentString(fileA) && isContentString(fileB)) {
+    return -1
+  } else if (isContentString(fileA) && !isContentString(fileB)) {
+    return 1
+  } else {
+    return 0
   }
 }
 
