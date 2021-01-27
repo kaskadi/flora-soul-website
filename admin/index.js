cffbc57913085223499ba76443f26f20435f7234
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-27 16:05:48 +0100

    Added authorization to protect admin pages

--- admin/index.js
+++ admin/index.js
@@ -1,3 +1,4 @@
+const verify = require('express-kaskadi-verify')('http://localhost:9192')
 const express = require('express')
 const app = express()
 const port = 3100
@@ -6,6 +7,8 @@ const { existsSync } = require('fs')
 
 app.set('view engine', 'pug')
 app.use(express.static('views'))
+
+app.use(verify({ issuer: ['flora-soul.com'] }))
 
 app.get('/', (req, res) => {
   res.render('index', {})
