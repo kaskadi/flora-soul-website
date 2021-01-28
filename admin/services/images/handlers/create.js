8d3238410caf16d425a0f11bcebc9f2225f43505
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-28 12:49:45 +0100

    Using file-type module for mime validation

--- admin/services/images/handlers/create.js
+++ admin/services/images/handlers/create.js
@@ -1,17 +1,10 @@
 const { writeFileSync, existsSync, mkdirSync } = require('fs')
 const sharp = require('sharp')
+const FileType = require('file-type')
 const passKey = require('./utils/pass-key.js')
 
-const mimeSignatures = {
-  'image/bmp': ['424d'],
-  'image/gif': ['47494638'],
-  'image/vnd.microsoft.icon': ['00000100'],
-  'image/jpeg': ['ffd8ffdb', 'ffd8ffee', 'ffd8ffe0', 'ffd8ffe1', 'ffd8ffe2', 'ffd8ffe3', 'ffd8ffe8'],
-  'image/png': ['89504e47'],
-  'image/svg+xml': [],
-  'image/tiff': ['492049', '49492a00', '4d4d002a', '4d4d002b'],
-  'image/webp': ['52494646', '57454250']
-}
+const imageMimes = ['bmp', 'gif', 'vnd.microsoft.icon', 'jpeg', 'png', 'svg+xml', 'tiff', 'webp'].map(mime => `image/${mime}`)
+const acceptedMimes = [...imageMimes]
 
 module.exports = async (req, res, next) => {
   const { key, content } = req.body
@@ -41,8 +34,8 @@ function createStructure (key) {
 async function writeFile (key, originalKey, content, res) {
   const fileContent = content.replace(/^data:[a-z]+\/[a-z]+;base64,/, '') // strip off base64 URL header from string to retrieve actual encoded data
   const bytes = Buffer.from(fileContent, 'base64')
-  const mime = getMime(bytes)
-  if (Object.keys(mimeSignatures).includes(mime) || isSvg(bytes)) {
+  const { mime } = await FileType.fromBuffer(bytes) || {} // we provide an empty object as fallback for when we can't detect mime type which happens mostly for non-binary files
+  if (acceptedMimes.includes(mime) || isSvg(bytes)) {
     await writeAsWebP(bytes, key, mime)
     res.status(201).send(`File ${key} successfully saved!`)
     writeFileSync(originalKey, fileContent, 'base64') // write into folder for original images
@@ -58,28 +51,6 @@ function isSvg (bytes) {
     binary += String.fromCharCode(bytes[i])
   }
   return binary.startsWith('<svg')
-}
-
-function checkMimeSignature (header) {
-  const compareSig = header => sig => {
-    const chars = header.split('').slice(0, sig.length) // we get characters from header and match to the signature length for checking
-    return !chars.some((char, i) => char !== sig[i])
-  }
-  for (const mime in mimeSignatures) {
-    if (mimeSignatures[mime].some(compareSig(header))) {
-      return mime
-    }
-  }
-  return null
-}
-
-function getMime (bytes) {
-  const headerArr = bytes.subarray(0, 4)
-  let header = ''
-  for (let i = 0; i < headerArr.length; i++) {
-    header += headerArr[i].toString(16)
-  }
-  return checkMimeSignature(header)
 }
 
 function writeAsWebP (bytes, key, mime) {
