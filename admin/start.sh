d60e5c117e4d99662c3ba29b304cea6e23044a82
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-02-15 11:29:43 +0100

    Updated start script

--- admin/start.sh
+++ admin/start.sh
@@ -4,4 +4,9 @@ kaskadi service install -n flora-soul-admin -e $PWD/index.js -r
 systemctl --user restart flora-soul-admin
 # starting images API
 cd services/images
-npm run start
+npm run install:prod
+npm run start:prod
+# starting fonts API
+cd services/fonts
+npm run install:prod
+npm run start:prod
