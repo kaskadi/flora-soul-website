8922bafb96944b01a270db6e106d7f8a488f39f8
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-02-15 11:31:53 +0100

    Cleaned up server entry file for fonts API

--- admin/services/fonts/index.js
+++ admin/services/fonts/index.js
@@ -1,45 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const WebSocket = require('ws')
-const port = 3110
-
-const getFiles = require('./handlers/utils/get-files.js')
-const { dirname, join } = require('path')
-
-require('dotenv').config({ path: join(__dirname, '.env') })
+const port = 3120
 
 app.use(express.json({ limit: '25mb', extended: false }))
 app.use(cors())
 
-process.chdir(process.env.ROOT)
+app.get('/', require('./handlers/convert.js'))
 
-app.get('/', require('./handlers/get.js'))
-app.get('/download', require('./handlers/download.js'))
-app.post('/create', require('./handlers/create.js'), broadcast)
-app.post('/rename', require('./handlers/rename.js'), broadcast)
-app.post('/delete', require('./handlers/delete.js'), broadcast)
-
-const server = app.listen(port, () => {
-  console.log(`Images API running http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`Fonts API running http://localhost:${port}`)
 })
-
-const wss = new WebSocket.Server({ server, path: '/ws' })
-
-async function broadcast (req, res, next) {
-  const { key } = res.locals
-  const dir = dirname(key) === '.' ? '' : dirname(key)
-  const files = await getFiles(dir)
-  const originalFiles = await getFiles(join('.originals', dir))
-  emitFiles(files, wss)
-  emitFiles(originalFiles, wss)
-  next()
-}
-
-function emitFiles (files, wss) {
-  for (const client of wss.clients) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(files))
-    }
-  }
-}
