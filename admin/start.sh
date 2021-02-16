a7cce04c53f57f867fdb7d071628cfee567ad383
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-02-16 13:49:28 +0100

    Fixed start script

--- admin/start.sh
+++ admin/start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # installing and starting admin platform
+npm i
 kaskadi service install -n flora-soul-admin -e $PWD/index.js -r
-systemctl --user restart flora-soul-admin
 # starting images API
 cd services/images
+npm i
 npm run install:prod
 npm run start:prod
 # starting fonts API
 cd services/fonts
+npm i
 npm run install:prod
 npm run start:prod
