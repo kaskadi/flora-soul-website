baa24901ea0b2e95a70b0ec574bea6ebc7da53cc
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-02-16 18:20:06 +0100

    Added authorization to font API

--- admin/services/fonts/index.js
+++ admin/services/fonts/index.js
@@ -1,3 +1,4 @@
+const verify = require('express-kaskadi-verify')('http://localhost:9192')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -5,6 +6,8 @@ const port = 3120
 
 app.use(express.json({ limit: '25mb', extended: false }))
 app.use(cors())
+
+app.use(verify({ issuer: ['flora-soul.com'], audience: ['api'] }))
 
 app.post('/', require('./handlers/convert.js'))
 app.get('/', require('./handlers/download.js'))
