52848a92f98a4baa9e3c0e994258f6ed176fbac6
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-02-16 18:20:58 +0100

    Using cookie to share token for API calls

--- admin/index.js
+++ admin/index.js
@@ -6,12 +6,19 @@ const port = 3100
 const { existsSync } = require('fs')
 const { join } = require('path')
 
+require('dotenv').config({ path: join(__dirname, '.env') })
+
 app.set('view engine', 'pug')
 const viewsPath = join(__dirname, 'views')
 app.set('views', viewsPath)
 app.use(express.static(viewsPath))
 
-app.use(verify({ issuer: ['flora-soul.com'] }))
+app.use(verify({ issuer: ['flora-soul.com'], audience: ['admin'] }))
+
+app.use((req, res, next) => {
+  res.clearCookie('API_KEY')
+  next()
+})
 
 app.get('/', (req, res) => {
   res.render('index', {})
@@ -21,9 +28,12 @@ app.get('/:view', (req, res) => {
   const { view } = req.params
   if (!existsSync(join(__dirname, 'views', `${view}.pug`))) {
     res.status(404).send('This page does not exist')
-  } else {
-    res.render(view, {})
+    return
   }
+  if (view !== 'index') {
+    res.cookie('API_KEY', process.env.API_TOKEN)
+  }
+  res.render(view, {})
 })
 
 app.listen(port, () => {
