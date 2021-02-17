5e6d3bafe1e7a64605c718c4bdedcf4bcc4d44ea
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-02-17 11:29:32 +0100

    Simplified code logic for rendering server

---
+++ admin/controllers/render.js
@@ -0,0 +1,25 @@
+const { existsSync } = require('fs')
+
+module.exports = (join) => (req, res) => {
+  const { view } = req.params
+  if (!existsSync(join(__dirname, '..', 'views', `${view}.pug`))) {
+    res.status(404).send('This page does not exist')
+    return
+  }
+  if (view !== 'index') {
+    res.cookie('API_KEY', process.env.API_TOKEN)
+  }
+  res.render(view, getViewData(view))
+}
+
+function getViewData (view) {
+  const { port, prodPath, hasWS } = require('../views/data/data.json').views[view].api
+  const isTest = process.env.NODE_ENV === 'dev'
+  const domain = isTest ? `localhost:${port}` : 'api.flora-soul.com'
+  const path = isTest ? '' : prodPath
+  const origin = `${domain}/${path}`
+  return {
+    apiUrl: `http://${origin}`,
+    ...hasWS && { wsApiUrl: `ws://${origin}` }
+  }
+}
--- admin/index.js
+++ admin/index.js
@@ -1,41 +1,26 @@
 const verify = require('express-kaskadi-verify')('http://localhost:9192')
 const express = require('express')
+const { join } = require('path')
 const app = express()
 const port = 3100
 
-const { existsSync } = require('fs')
-const { join } = require('path')
-
 require('dotenv').config({ path: join(__dirname, '.env') })
 
+// rendering engine settings
 app.set('view engine', 'pug')
 const viewsPath = join(__dirname, 'views')
 app.set('views', viewsPath)
 app.use(express.static(viewsPath))
 
+// authorization
 app.use(verify({ issuer: ['flora-soul.com'], audience: ['admin'] }))
 
-app.use((req, res, next) => {
-  res.clearCookie('API_KEY')
-  next()
-})
+// clear cookies
+app.use(require('./middlewares/clear-cookies.js'))
 
-app.get('/', (req, res) => {
-  res.render('index', {})
-})
-
-app.get('/:view', (req, res) => {
-  const { view } = req.params
-  if (!existsSync(join(__dirname, 'views', `${view}.pug`))) {
-    res.status(404).send('This page does not exist')
-    return
-  }
-  if (view !== 'index') {
-    res.cookie('API_KEY', process.env.API_TOKEN)
-  }
-  const isTest = process.env.NODE_ENV === 'dev'
-  res.render(view, { isTest })
-})
+// endpoints
+app.get('/', require('./controllers/render-root.js'))
+app.get('/:view', require('./controllers/render.js')(join))
 
 app.listen(port, () => {
   console.log(`Admin page hosted at http://localhost:${port}. Don't forget to start all related APIs!`)
